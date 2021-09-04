e6c715dc32f43b6c5d71f1d0854043146429137b
Nandu064 <harish861432@gmail.com>
2021-09-04 19:56:07 +0530

    added ant-d styles to index.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import { Button } from 'antd';
 function App() {
   return (
     <div className="App">
-        Hi I'm React App
+        <Button type="primary">Hi I'm React App</Button>
     </div>
   );
 }
