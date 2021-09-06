8bff7f1253e2bdcaeee9943fe9957f0849127cdd
Nandu064 <harish861432@gmail.com>
2021-09-06 14:54:16 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
         <header className="header">
           <nav>
           <ul>
-          <li><a href="/login">Login</a></li>
+          <li><a href="/">Login</a></li>
           <li><a href="/date">Date</a></li>
           <li><a href="/video">Video</a></li>
           </ul>
           </nav>
         </header>
             <Switch>
-              <Route path="/login" >
+              <Route exact path="/" >
                 <Login />
               </Route>
               <Route path="/video" >
