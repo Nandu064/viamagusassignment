5c782b6fb8876ef37bc2c36a83c36b15671f0871
Nandu064 <harish861432@gmail.com>
2021-09-07 14:42:04 +0530

    commited

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -5,9 +5,11 @@ import './Video.css'
 function Video() {
     return (
         <div className="outer_wrapper">
-            <div className="circle"></div>
-            <div className="thumbNail">
-                <img src="http://img.youtube.com/vi/ysz5S6PUM-U/0.jpg" style={{width:'100px',height:'100px'}}/>
+            <div className="circle">
+
+                <img src="http://img.youtube.com/vi/ysz5S6PUM-U/0.jpg" style={{width:'50px',height:'50px'}}/>
+            {/* <div className="thumbNail">
+            </div> */}
             </div>
             <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U'/>
         </div>
